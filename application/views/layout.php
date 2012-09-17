947c0e46ca927f91fac514a4a0cee2325a3a5cc2
Adam Becker <adam@presidentialinnovationfellows.org>
2012-09-17 09:39:30 -0400

    add google analytics

--- application/views/layout.php
+++ application/views/layout.php
@@ -64,7 +64,7 @@
   <?= HTML::script('js/bootstrap.js') ?>
 
   <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    var _gaq=[['_setAccount','UA-34873942-1'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
